8584933823787d365b1d204c00036935c1d2a86f
Jiyeong Jeong <8533283@naver.com>
2023-06-05 12:16:59 +0900

    feat: 다이어리 수정 기능

--- src/routes/logRouter.js
+++ src/routes/logRouter.js
@@ -3,42 +3,48 @@ import { logService } from "../services/index.js";
 
 const logRouter = Router();
 
-logRouter.post("/log/:userId", async(req, res, next) => {
-    // res.header("Access-Control-Allow-origin", "*");
-    try {
-        const { userId } = req.params;
+logRouter.post("/log/:userId", async (req, res, next) => {
+  // res.header("Access-Control-Allow-origin", "*");
+  try {
+    const { userId } = req.params;
 
-        await logService.createLog({...req.body, author: userId });
-        res.status(201).end();
-    } catch (err) {
-        next(err);
-    }
+    await logService.createLog({ ...req.body, author: userId });
+    res.status(201).end();
+  } catch (err) {
+    next(err);
+  }
 });
 
-logRouter.get("/log/all/:userId", async(req, res, next) => {
-    // res.header("Access-Control-Allow-origin", "*");
-    try {
-        const { userId } = req.params;
+logRouter.put("/log/:logId", async (req, res, next) => {
+  const newLog = await logService.updateLog(req.params.logId, req.body);
 
-        const logs = await logService.findLogsByUserId(userId);
-
-        res.status(200).send(logs);
-    } catch (err) {
-        next(err);
-    }
+  res.status(201).json(newLog);
 });
 
-logRouter.get("/log/:logId", async(req, res, next) => {
-    // res.header("Access-Control-Allow-origin", "*");
-    try {
-        const { logId } = req.params;
+logRouter.get("/log/all/:userId", async (req, res, next) => {
+  // res.header("Access-Control-Allow-origin", "*");
+  try {
+    const { userId } = req.params;
 
-        const log = await logService.findLog(logId);
+    const logs = await logService.findLogsByUserId(userId);
 
-        res.status(200).send(log);
-    } catch (err) {
-        next(err);
-    }
+    res.status(200).send(logs);
+  } catch (err) {
+    next(err);
+  }
+});
+
+logRouter.get("/log/:logId", async (req, res, next) => {
+  // res.header("Access-Control-Allow-origin", "*");
+  try {
+    const { logId } = req.params;
+
+    const log = await logService.findLog(logId);
+
+    res.status(200).send(log);
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default logRouter;
